eb9d157c5261b71197919dc5c366db8d37e4f8eb
w411e <v.kniesel@posteo.de>
2025-09-24 17:03:19 +0200

    initial commit, that works inside vscode

--- src/main.c
+++ src/main.c
@@ -61,7 +61,7 @@ int main(int argc, char **argv)
   lv_init();
 
   /*Initialize the HAL (display, input devices, tick) for LVGL*/
-  sdl_hal_init(320, 480);
+  sdl_hal_init(551, 800);
 
   /* Run the default demo */
   /* To try a different demo or example, replace this with one of: */
@@ -76,7 +76,7 @@ int main(int argc, char **argv)
      * It could be done in a timer interrupt or an OS task too.*/
     uint32_t sleep_time_ms = lv_timer_handler();
     if(sleep_time_ms == LV_NO_TIMER_READY){
-	sleep_time_ms =  LV_DEF_REFR_PERIOD;
+	    sleep_time_ms =  LV_DEF_REFR_PERIOD;
     }
 #ifdef _MSC_VER
     Sleep(sleep_time_ms);
